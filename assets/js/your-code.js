a942081fadc725983f153435e517762933c62151
Dom <dtaplin21@aol.com>
2023-07-25 20:45:37 -0700

    fetch

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,43 @@
 export function getAllDogs() {
-    // Your code here
+
+return fetch("/dogs")
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+   return fetch("/dogs/2")
 }
 
 export function postNewDog() {
-    // Your code here
+    return fetch("/dogs", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchparams({
+            name: "Jared",
+            age: 5
+        })
+    })
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+     return fetch("/dogs", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            name: 'jessie',
+            age: 10
+        })
+     })
 }
 
 export function deleteDog(id) {
-      // Your code here
+      return fetch(`/dogs/${id}/delete`, {
+        method: "POST",
+        header: {
+            AUTH: "ckyut5wau0000jyv5bsrud90y"
+        },
+      });
 }
